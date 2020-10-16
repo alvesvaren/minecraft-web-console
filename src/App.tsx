247819f66d1b027963197378c05960a07a022e88
Alve Svarén <alve@hotmail.se>
2020-10-16 20:09:11 +0200

    Added basic layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,24 @@ class App extends React.Component<AppProps, AppState> {
         this.state = {};
     }
     render() {
-        return <div></div>;
+        return (
+            <div className="app-root">
+                <header id="status-area"></header>
+                <main id="content-area">
+                    <section id="console-area"><textarea readOnly id="console-field"></textarea></section>
+                    <section id="sidebar-area">
+                        <ul>
+                            <li>Player 1</li>
+                        </ul>
+                    </section>
+                </main>
+
+                <form id="command-area" onSubmit={e => e.preventDefault()}>
+                    <input id="command-field" type="text" placeholder="command or ? for help" />
+                    <input type="submit" value="Send"/>
+                </form>
+            </div>
+        );
     }
 }
 
