dd7d7f78bda7d2f741ab770481437a4763a1d9f9
Alve Svarén <alve@hotmail.se>
2020-10-16 18:23:17 +0200

    Updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from "react";
-import "./App.css";
+import "./app.scss";
 
+export interface AppProps {}
+
+export interface AppState {}
+
+class App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return <div></div>;
+    }
+}
 
 export default App;
